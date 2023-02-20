6c22d026f3057253933d6f50c0adca5dc4d9d543
Travis Bird <tboydlink@gmail.com>
2023-02-19 21:23:22 -0600

    UI????

--- Assets/_Assets/Scripts/Hitbox.cs
+++ Assets/_Assets/Scripts/Hitbox.cs
@@ -6,7 +6,7 @@ public class Hitbox : MonoBehaviour
 {
     [SerializeField] private bool destroyOnHit;
     public bool DestroyOnHit => destroyOnHit;
-    private float damage;
+    [SerializeField] private float damage;
     public float Damage => damage;
     public void SetDamage(float _dmg)
     {
